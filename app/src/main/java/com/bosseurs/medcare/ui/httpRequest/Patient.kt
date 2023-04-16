21d65a2af5c640f574436eb05e03d10dbfeea717
zawa9 <zakaria.bourhil@um5r.ac.ma>
2023-04-16 04:43:48 +0000

    connecting the backend Patient Model with app

---
+++ app/src/main/java/com/bosseurs/medcare/ui/httpRequest/Patient.kt
@@ -0,0 +1,9 @@
+package com.bosseurs.medcare.ui.httpRequest
+
+data class Patient(
+    val first_name: String,
+    val last_name: String,
+    val cin: String,
+    val num: String,
+    val password: String
+)
---
+++ app/src/main/java/com/bosseurs/medcare/ui/httpRequest/PatientService.kt
@@ -0,0 +1,18 @@
+package com.bosseurs.medcare.ui.httpRequest
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface RetrofitAPI {
+    @POST("register")
+    fun register(@Body patient: Patient?): Call<Patient?>?
+
+    @FormUrlEncoded
+    @POST("login")
+    fun login(
+        @Field("cin") cin: String,
+        @Field("password") password: String
+    ): Call<Patient?>?
+}
+
+
